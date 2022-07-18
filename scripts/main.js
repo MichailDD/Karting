bfcccc33d6b0f0fa607eae13f9aecf377375dd12
MichailDD <29307348+MichailDD@users.noreply.github.com>
2022-07-18 18:20:58 +0300

    test
    test

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  slidesPerView:'auto',
+  spaceBetween:85,
+  initialSlide:1
+});
